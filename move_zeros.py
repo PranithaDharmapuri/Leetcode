e1161f271307c2576f6eb44103489998ebdf61ee
Pranitha Dharmapuri <pranithadharmapuri29@gmail.com>
2025-07-23 14:09:44 +0530

    demo commit

--- move_zeros.py
+++ move_zeros.py
@@ -1,5 +1,20 @@
+from typing import List
+
+
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+    def moveZeroes(self, nums: List[int]) -> int:
         """
         Do not return anything, modify nums in-place instead.
         """
+        k = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[k] = nums[k], nums[i]
+                k = k+1
+        return nums
+
+
+obj = Solution()
+ans = obj.moveZeroes(nums=[0, 1, 0, 3, 12])
+print(ans)
--- remove_duplicates.py
+++ remove_duplicates.py
@@ -6,16 +6,17 @@ class Solution(object):
         """
         if not nums:
             return 0
-        k=0
-        for i in range(0,len(nums)):
-            nums[i]=nums[i]*nums[i]
-            nums[k]=nums[i]
-            k=k+1
+        k = 0
+        for i in range(0, len(nums)):
+            nums[i] = nums[i]*nums[i]
+            nums[k] = nums[i]
+            k = k+1
         nums.sort()
         return nums
 
-nums=[-2,0,2,5,4]
-obj=Solution()
 
-ans=obj.sortedSquares(nums)
+nums = [-2, 0, 2, 5, 4]
+obj = Solution()
+
+ans = obj.sortedSquares(nums)
 print(ans)
