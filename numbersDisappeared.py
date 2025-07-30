1e6fa70c9a833757fbab97f6e7f484b6322c1c38
Pranitha Dharmapuri <pranithadharmapuri29@gmail.com>
2025-07-30 21:55:51 +0530

    commit

--- numbersDisappeared.py
+++ numbersDisappeared.py
@@ -3,26 +3,15 @@ from typing import List
 
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        minValue = min(nums)
-        maxValue = max(nums)
-        missedValues = []
-        if minValue == maxValue:
-            if minValue == 1:
-                missedValues.append(minValue+1)
-            elif maxValue > 1:
-                for i in range(1, maxValue):
-                    missedValues.append(i)
-        else:
-            for i in range(minValue, maxValue+1):
-                if i not in nums:
-                    missedValues.append(i)
-                elif nums.count(i) > 1:
-                    continue
-
-        return missedValues
+        set_nums = set(nums)
+        missing = []
+        for i in range(1, len(nums)+1):
+            if i not in nums:
+                missing.append(i)
+        return missing
 
 
-myList = list(map(int, input().split(',')))
+myList = list(map(int, input().split(' ')))
 obj = Solution()
 res = obj.findDisappearedNumbers(myList)
 print(res)
