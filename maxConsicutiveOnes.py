36a25132841a727be96febb08b5da0d32286f8c6
Pranitha Dharmapuri <pranithadharmapuri29@gmail.com>
2025-08-06 23:42:44 +0530

    modified

--- maxConsicutiveOnes.py
+++ maxConsicutiveOnes.py
@@ -16,22 +16,6 @@ class Solution:
         return maxCount
 
 
-"""
-#trick 2
-
-class Solution:
-    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        maxCount = 0
-        count = 0
-        for num in nums:
-            if num == 1:
-                count += 1
-                maxCount = max(count, maxCount)
-            else:
-                count = 0
-        return maxCount"""
-
-
 myList = list(map(int, input().split()))
 obj = Solution()
 ans = obj.findMaxConsecutiveOnes(myList)
