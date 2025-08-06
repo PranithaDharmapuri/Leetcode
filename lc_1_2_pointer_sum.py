7e0e53c320611d9b0961276b8d7c056e7ce3e61a
Pranitha Dharmapuri <pranithadharmapuri29@gmail.com>
2025-08-06 23:41:31 +0530

    modified

--- lc_1_2_pointer_sum.py
+++ lc_1_2_pointer_sum.py
@@ -1,17 +1,17 @@
-#from typing import List
+# from typing import List
 class Solution:
     def twoSum(self, numbers, target):
         left, right = 0, len(numbers) - 1
 
         while left < right:
-            if numbers[left]+numbers[right]==target:
-                return [left+1,right+1]
-            elif numbers[left]+numbers[right]<target:
+            if numbers[left]+numbers[right] == target:
+                return [left+1, right+1]
+            elif numbers[left]+numbers[right] < target:
                 left += 1
             else:
                 right -= 1
 
 
-obj=Solution()
-print(obj.twoSum([2,7,11,15],9))
-
+obj = Solution()
+res = obj.twoSum([2, 7, 11, 15], 9)
+print(res)
