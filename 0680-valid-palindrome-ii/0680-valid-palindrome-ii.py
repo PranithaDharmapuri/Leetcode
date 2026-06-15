b2297782833ec29bd43d4749d2e92c9d5659bb0c
PranithaDharmapuri <pranithadharmapuri29@gmail.com>
2026-06-15 18:47:52 +0530

    Time: 90 ms (21.48%), Space: 12.5 MB (96.97%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        def check(l,r):
+            while l<r:
+                if s[l]!=s[r]:
+                    return False
+                l+=1
+                r-=1
+            return True
+    
+
+        l=0
+        r=len(s)-1
+
+        while l<r:
+            if s[l]==s[r]:
+                l+=1
+                r-=1
+
+            else:
+                return check(l+1,r) or check(l,r-1)
+
+
+        if l>=r:
+                return True
+            
+
+
+    
+        
